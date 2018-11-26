4ddb054ad11067b5deb4776c47e3d400770a7ad2
mzh <manzhenhua@sunlands.com>
2018-11-26 14:48:22 +0800

    修改端口

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.prepare().then(() => {
     return handle(req, res)
   })
 
-  server.listen(4000, (err) => {
+  server.listen(3000, (err) => {
     if (err) throw err
     console.log('> Ready on http://localhost:3000')
     console.log(process)
